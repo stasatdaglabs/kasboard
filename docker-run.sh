ed22534018c14f215c6f762fdeb018fe102b6074
stasatdaglabs <stas@daglabs.com>
2021-02-18 13:54:22 +0200

    Add a Dockerfile for the web stuff.

--- docker-run.sh
+++ docker-run.sh
@@ -3,11 +3,13 @@
 set -e
 
 docker build -f processing/Dockerfile -t kashboard-processing:latest .
+docker build -f web/Dockerfile -t kashboard-web:latest .
 docker-compose up -d postgres
 docker-compose up -d kaspad
 
 sleep 10s
 
 docker-compose up -d processing
+docker-compose up -d web
 
 docker-compose logs -f
