39b1593abf893dae8c8de52b30b4a3b26fe8fd53
stasatdaglabs <stas@daglabs.com>
2021-02-21 15:49:16 +0200

    Remove the web stuff, create grafana/postgres.yaml.

--- docker-run.sh
+++ docker-run.sh
@@ -3,15 +3,16 @@
 set -e
 
 docker build -f processing/Dockerfile -t kashboard-processing:latest .
-docker build -f web/Dockerfile -t kashboard-web:latest .
 docker build -f grafana/Dockerfile -t kashboard-grafana:latest .
 docker-compose up -d postgres
 docker-compose up -d kaspad
-docker-compose up -d grafana
 
 sleep 10s
 
 docker-compose up -d processing
-docker-compose up -d web
+
+sleep 10s
+
+docker-compose up -d grafana
 
 docker-compose logs -f
--- web/scripts/date.ts
+++
@@ -1,6 +0,0 @@
-import dateFormat from "dateformat";
-
-export const timestampToString = (timestamp: number) => {
-    const date = new Date(timestamp);
-    return dateFormat(date, "yyyy-mm-dd HH:MM:ss");
-};
