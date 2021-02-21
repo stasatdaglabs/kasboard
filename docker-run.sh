54c0f900cc66afaaeab106bec8377aa07c3d2397
stasatdaglabs <stas@daglabs.com>
2021-02-21 14:24:09 +0200

    Add grafana along with an appropriate Dockerfile.

--- docker-run.sh
+++ docker-run.sh
@@ -4,8 +4,10 @@ set -e
 
 docker build -f processing/Dockerfile -t kashboard-processing:latest .
 docker build -f web/Dockerfile -t kashboard-web:latest .
+docker build -f grafana/Dockerfile -t kashboard-grafana:latest .
 docker-compose up -d postgres
 docker-compose up -d kaspad
+docker-compose up -d grafana
 
 sleep 10s
 
