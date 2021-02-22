3fbf31a883144896623a0d7ec506337b0ed76673
stasatdaglabs <stas@daglabs.com>
2021-02-22 11:56:07 +0200

    Verify that all the required environment variables are set.

--- docker-run.sh
+++ docker-run.sh
@@ -2,6 +2,32 @@
 
 set -e
 
+# Verify that all the required environment variables are set
+declare -A REQUIRED_VARIABLES
+REQUIRED_VARIABLES["POSTGRES_USER"]="${POSTGRES_USER}"
+REQUIRED_VARIABLES["POSTGRES_PASSWORD"]="${POSTGRES_PASSWORD}"
+REQUIRED_VARIABLES["POSTGRES_DB"]="${POSTGRES_DB}"
+REQUIRED_VARIABLES["KASPAD_ADDRESS"]="${KASPAD_ADDRESS}"
+REQUIRED_VARIABLES["GRAFANA_ADMIN_USERNAME"]="${GRAFANA_ADMIN_USERNAME}"
+REQUIRED_VARIABLES["GRAFANA_ADMIN_PASSWORD"]="${GRAFANA_ADMIN_PASSWORD}"
+
+REQUIRED_VARIABLE_NOT_SET=false
+for REQUIRED_VARIABLE_NAME in "${!REQUIRED_VARIABLES[@]}"; do
+  if [ -z "${REQUIRED_VARIABLES[$REQUIRED_VARIABLE_NAME]}" ]; then
+    echo "${REQUIRED_VARIABLE_NAME} is not set";
+    REQUIRED_VARIABLE_NOT_SET=true
+    fi
+done
+
+if [ true = "${REQUIRED_VARIABLE_NOT_SET}" ]; then
+  echo
+  echo "The following environment variables are required:"
+  for REQUIRED_VARIABLE_NAME in "${!REQUIRED_VARIABLES[@]}"; do
+    echo "${REQUIRED_VARIABLE_NAME}"
+  done
+  exit 1
+fi
+
 # Build the grafana and processing images
 docker build -f processing/Dockerfile -t kashboard-processing:latest .
 docker build -f grafana/Dockerfile -t kashboard-grafana:latest .
