765788975c5acde2ddff6bbe700cb68fd965096c
stasatdaglabs <stas@daglabs.com>
2021-02-22 10:31:18 +0200

    Remove kaspad from docker-compose.yaml.

--- docker-run.sh
+++ docker-run.sh
@@ -2,17 +2,24 @@
 
 set -e
 
+# Build the grafana and processing images
 docker build -f processing/Dockerfile -t kashboard-processing:latest .
 docker build -f grafana/Dockerfile -t kashboard-grafana:latest .
-docker-compose up -d postgres
-docker-compose up -d kaspad
 
+# Start postgres
+docker-compose up -d postgres
+
+# Wait for postgres to finish initializing
 sleep 10s
 
+# Start processing
 docker-compose up -d processing
 
+# Wait for processing to finish initializing
 sleep 10s
 
+# Start grafana
 docker-compose up -d grafana
 
+# Print logs for all services
 docker-compose logs -f
